b96f812fe876669c8cc2eae7fee01d2fa21f6088
MIHIR2006 <mihirgoswami2006@gmail.com>
2025-11-29 00:45:50 +0530

    feat(ui): landing.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import {HeroGeometric} from "./Hero";
+
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <HeroGeometric badge = "Coming soon" title1="Crystallize your" title2="code and thoughts"/>
+    </div>
+  );
+}
